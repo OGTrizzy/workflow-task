424053024690fd4ed496f43443fd9291171a0b46
Tristian Øyen <142795941+OGTrizzy@users.noreply.github.com>
2024-10-17 01:40:37 +0200

    made changes to login.spec.cy.js

--- cypress/e2e/integration/login.spec.cy.js
+++ cypress/e2e/integration/login.spec.cy.js
@@ -1,22 +1,58 @@
-describe('Login Tests', () => {
-  it('should log in with valid credentials', () => {
-    cy.visit('/');
-    cy.get('input[name=username]').type('validUsername');
-    cy.get('input[name=password]').type('validPassword');
-    cy.get('button[type=submit]').click();
-    cy.url().should('include', '/');
+import { apiPath } from '../../../src/js/api';
+
+describe('Login and Logout Tests', () => {
+  beforeEach(() => {
+    cy.visit('/index.html');
+
+    cy.get('body').then(($body) => {
+      if ($body.find('#closeButton').length > 0) {
+        cy.get('#closeButton').click({ force: true });
+      }
+    });
   });
-});
 
-it('should show a message when invalid credentials are provided', () => {
-  cy.visit('/');
-  cy.get('input[name=username]').type('invalidUsername');
-  cy.get('input[name=password]').type('invalidPassword');
-  cy.get('button[type=submit]').click();
-  cy.get('.error-message').should('be.visible');
-});
+  it('should login successfully with correct credentials', () => {
+    cy.get('#loginEmail').type('ogtrizzy420@stud.noroff.no');
+    cy.get('#loginPassword').type('111222333');
 
-it('should log out when the logout button is clicked', () => {
-  cy.get('button#logout').click();
-  cy.url().should('include', '/');
+    cy.intercept('POST', `${apiPath}/social/auth/login`).as('loginAttempt');
+
+    cy.get('#loginForm').submit();
+
+    cy.wait('@loginAttempt').its('response.statusCode').should('eq', 200);
+  });
+
+  it('should not login with incorrect credentials and should display error message', () => {
+    cy.on('window:alert', (str) => {
+      expect(str).to.equal('Your password or email is incorrect');
+    });
+
+    cy.get('#loginEmail').type('ogtrizzy420@noroff.no');
+    cy.get('#loginPassword').type('wrongpassword');
+
+    cy.intercept('POST', `${apiPath}/social/auth/login`).as('loginAttempt');
+    cy.get('#loginForm').submit();
+
+    cy.wait('@loginAttempt').its('response.statusCode').should('eq', 401);
+  });
+
+  it('should logout user successfully', () => {
+    cy.get('#loginEmail').type('ogtrizzy420@stud.noroff.no');
+    cy.get('#loginPassword').type('111222333');
+
+    cy.intercept('POST', `${apiPath}/social/auth/login`).as('loginAttempt');
+    cy.get('#loginForm').submit();
+
+    cy.wait('@loginAttempt').its('response.statusCode').should('eq', 200);
+
+    cy.window().then((window) => {
+      expect(window.localStorage.getItem('token')).to.exist;
+    });
+
+    cy.get('#logoutButton').should('be.visible').click();
+
+    cy.window().then((window) => {
+      expect(window.localStorage.getItem('token')).to.be.null;
+    });
+  });
 });
